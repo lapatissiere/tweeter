9b7412a2547434fce5871d579eab18b290113a61
Nazia Farooq <lapatissiere@yahoo.com>
2023-05-17 21:31:26 -0400

    edited to correct character counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,12 +3,18 @@ $(document).ready(function () {
 
   //registering an event handler to the textarea element
 
-    $('.new-tweet textarea').on('input', function() {
-      const maxLength = 140; // Maximum character count allowed
-      const currentLength = $(this).val().length; // Get the current length of the textarea's value
-      const remainingLength = maxLength - currentLength; // Calculate the remaining characters
-      
-      // Update the character counter display
-      $('.new-tweet .counter').text(remainingLength);
-    });
+  $(".new-tweet textarea").on("input", function () {
+    const maxLength = 140; // Maximum character count allowed
+    const currentLength = $(this).val().length; // Get the current length of the textarea's value
+    const remainingLength = maxLength - currentLength; // Calculate the remaining characters
+
+    const counterElement = $(".new-tweet .counter");
+    counterElement.text(remainingLength);
+
+    if (remainingLength < 0) {
+      counterElement.addClass("red");
+    } else {
+      counterElement.removeClass("red");
+    }
   });
+});
