f597433de425b6c83a177efcdd0ec0fdb33dbe85
Nazia Farooq <lapatissiere@yahoo.com>
2023-05-18 18:09:11 -0400

    character counter corrected, word-break implemented in new-tweet.css, responsive design refined

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-  console.log("Here I am!");
-
-  //registering an event handler to the textarea element
-
+  // Registering an event handler to the textarea element
   $(".new-tweet textarea").on("input", function () {
     const maxLength = 140; // Maximum character count allowed
     const currentLength = $(this).val().length; // Get the current length of the textarea's value
@@ -16,5 +13,26 @@ $(document).ready(function () {
     } else {
       counterElement.removeClass("red");
     }
+  });
+
+  // Reset the counter when the textarea is cleared or its value is set programmatically
+  $(".new-tweet textarea").on("change", function () {
+    const counterElement = $(".new-tweet .counter");
+    const maxLength = 140;
+    const currentLength = $(this).val().length;
+
+    if (currentLength === 0) {
+      counterElement.text(maxLength);
+      counterElement.removeClass("red");
+    }
+  });
+
+  // Reset the counter after submitting the tweet
+  $(".new-tweet form").on("submit", function () {
+    const counterElement = $(".new-tweet .counter");
+    const maxLength = 140;
+    
+    counterElement.text(maxLength);
+    counterElement.removeClass("red");
   });
 });
